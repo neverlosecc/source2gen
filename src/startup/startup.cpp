06e2482d53b16ee77dbb4d86a6d968b61db0cfbf
Arsenii es3n1n <40367813+es3n1n@users.noreply.github.com>
2023-03-23 03:54:49 +0100

    refactor(startup): minor refactor

--- src/startup/startup.cpp
+++ src/startup/startup.cpp
@@ -3,36 +3,57 @@
 
 #include "tools/console/console.h"
 
+namespace
+{
+	using namespace std::string_view_literals;
+
+	constexpr std::initializer_list<std::string_view> kRequiredGameModules = {
+		"client.dll"sv,
+		"engine2.dll"sv,
+		"schemasystem.dll"sv
+	};
+}
+
 namespace source2_gen
 {
 	std::atomic_bool is_finished = false;
 
 	void Setup()
 	{
-		while (!GetModuleHandleA("GameOverlayRenderer64.dll") ||
-			!GetModuleHandleA("client.dll") ||
-			!GetModuleHandleA("engine2.dll") ||
-			!GetModuleHandleA("steamclient64.dll") ||
-			!GetModuleHandleA("schemasystem.dll"))
+		// @note: @es3n1n: Waiting for game init
+		//
+		const auto required_modules_present = []() [[msvc::forceinline]] -> bool {
+			bool result = true;
+
+			for (auto& name : kRequiredGameModules)
+				result &= static_cast<bool>(GetModuleHandleA(name.data()));
+
+			return result;
+		};
+
+		while (!required_modules_present())
 			sleep_for(std::chrono::seconds(5));
 
-		fmt::print("{}: Found GameOverlayRenderer64\n", __FUNCTION__);
+		fmt::print("{}: Starting\n", __FUNCTION__);
 
-		sdk::g_schema = sdk::GetInterface<CSchemaSystem>("schemasystem.dll", "SchemaSystem_0");
-		if (!sdk::g_schema) { throw std::runtime_error(fmt::format("Failed to obtain Schema interfaces.")); }
+		// @note: @es3n1n: Capture interfaces
+		//
+		sdk::g_schema = CSchemaSystem::GetInstance();
+		if (!sdk::g_schema)
+			throw std::runtime_error(fmt::format("Unable to obtain Schema interface"));
 
-		fmt::print("{}: Dumping SDK for Source 2\n", __FUNCTION__);
-
-		// Generate SDK
+		// @note: @es3n1n: Obtaining type scopes and generating sdk
+		//
 		const auto type_scopes = sdk::g_schema->GetTypeScopes();
 		for (auto i = 0; i < type_scopes.Count(); ++i)
-		{
-			const auto& current = type_scopes.m_pElements[i];
-			sdk::GenerateTypeScopeSdk(current);
-		}
+			sdk::GenerateTypeScopeSdk(type_scopes.m_pElements[i]);
 
+		// @note: @es3n1n: Generating sdk for global type scope
+		//
 		sdk::GenerateTypeScopeSdk(sdk::g_schema->GlobalTypeScope());
 
+		// @note: @es3n1n: We are done here
+		//
 		is_finished = true;
 	}
 
@@ -50,8 +71,10 @@ namespace source2_gen
 		}
 
 		fmt::print("Successfuly dumped Source 2 SDK, now you can safely close this console.\nVisit: neverlose.cc!\n");
+
 		console->stop();
 		console.reset();
+
 		FreeLibraryAndExitThread(module, EXIT_SUCCESS);
 	}
 } // namespace source2gen
