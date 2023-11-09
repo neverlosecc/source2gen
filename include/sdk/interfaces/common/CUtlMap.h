bfcfb5bffce60ec6fd653f5bf5f7d727ab0c7614
es3n1n <me@es3n.in>
2023-11-09 21:57:00 +0700

    feat(scripts): add `update_copyrights.py`

--- include/sdk/interfaces/common/CUtlMap.h
+++ include/sdk/interfaces/common/CUtlMap.h
@@ -1,3 +1,5 @@
+// Copyright (C) 2023 neverlosecc
+// See end of file for extended copyright information.
 #pragma once
 
 template <typename S, typename T>
@@ -54,3 +56,20 @@ const T& CUtlMap<S, T>::Element(int i) const {
     assert(i < size_);
     return base_[i];
 };
+
+
+// source2gen - Source2 games SDK generator
+// Copyright 2023 neverlosecc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
---
+++ scripts/update_copyrights.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from datetime import datetime
+from typing import List
+
+
+EXTENSIONS = ['.cpp', '.cc', '.cxx', '.c', '.hpp', '.h']
+
+
+current_dir = Path(__file__).parent
+current_year = str(datetime.utcnow().year)
+
+
+def format_buffer(s: str) -> str:
+    return s.replace('%year%', current_year)
+
+
+with open(current_dir / 'license_template.txt', 'r') as f:
+    license_template = format_buffer(f.read())
+
+with open(current_dir / 'license_template_header.txt', 'r') as f:
+    license_template_header = format_buffer(f.read())
+
+
+def get_comment_chr(file_name: str) -> str:
+    return '#' if file_name.endswith('.py') else '//'
+
+
+def to_comments(file_name: str, buffer: str) -> str:
+    comment_chr: str = get_comment_chr(file_name)
+    return '\n'.join([f'{comment_chr} {line}'.strip() for line in buffer.splitlines()])
+
+
+def remove_copyright(file_name: str, lines: List[str]) -> List[str]:
+    comment_chr: str = get_comment_chr(file_name)
+
+    ends_at: int = 0
+    for i, line in enumerate(lines):
+        if line.startswith(comment_chr):
+            continue
+
+        ends_at = max(0, i)
+        break
+
+    starts_at_bottom: int = len(lines) - 1
+    for i, line in reversed(list(enumerate(lines))):
+        if line.startswith(comment_chr) or line == '':
+            continue
+
+        starts_at_bottom = min(len(lines) - 1, i + 1)
+        break
+
+    return lines[ends_at:starts_at_bottom]
+
+
+def append_copyright_to(p: Path) -> None:
+    header = to_comments(p.name, license_template_header).splitlines()
+    full = to_comments(p.name, license_template).splitlines()
+
+    with open(p, 'r') as f:
+        file_lines = f.read().splitlines()
+
+    def _is_copyrighted() -> bool:
+        return file_lines[0].split(current_year)[0] == header[0].split(current_year)[0]
+    
+    is_copyrighted = _is_copyrighted()
+    if is_copyrighted:
+        file_lines = remove_copyright(p.name, file_lines)
+
+    file_lines = header + file_lines + ['\n'] + full
+    with open(p, 'w') as f:
+        f.write('\n'.join(file_lines))
+
+
+def iter_dir(p: Path) -> None:
+    for file in p.iterdir():
+        if file.is_dir():
+            iter_dir(file)
+            continue
+
+        for ext in EXTENSIONS:
+            if not file.name.endswith(ext):
+                continue
+
+            append_copyright_to(file)
+            break
+
+
+if __name__ == '__main__':
+    iter_dir(Path(__file__).parent.parent)
+
