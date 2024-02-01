ea4da5d5650f64cc215f7e5997cfe197713ccb98
Arsenii es3n1n <me@es3n.in>
2024-02-01 20:20:23 +0100

    style: reformat

--- src/sdk/sdk.cpp
+++ src/sdk/sdk.cpp
@@ -16,26 +16,28 @@ namespace {
     constexpr size_t kMinFieldCountForClassEmbed = 2;
     constexpr size_t kMaxFieldCountForClassEmbed = 12;
 
-    constinit std::array string_metadata_entries = {FNV32("MNetworkChangeCallback"),
-                                                    FNV32("MPropertyFriendlyName"),
-                                                    FNV32("MPropertyDescription"),
-                                                    FNV32("MPropertyAttributeRange"),
-                                                    FNV32("MPropertyStartGroup"),
-                                                    FNV32("MPropertyAttributeChoiceName"),
-                                                    FNV32("MPropertyGroupName"),
-                                                    FNV32("MNetworkUserGroup"),
-                                                    FNV32("MNetworkAlias"),
-                                                    FNV32("MNetworkTypeAlias"),
-                                                    FNV32("MNetworkSerializer"),
-                                                    FNV32("MPropertyAttributeEditor"),
-                                                    FNV32("MPropertySuppressExpr"),
-                                                    FNV32("MKV3TransferName"),
-                                                    FNV32("MFieldVerificationName"),
-                                                    FNV32("MVectorIsSometimesCoordinate"),
-                                                    FNV32("MNetworkEncoder"),
-                                                    FNV32("MPropertyCustomFGDType"),
-                                                    FNV32("MVDataUniqueMonotonicInt"),
-                                                    FNV32("MScriptDescription")};
+    constinit std::array string_metadata_entries = {
+        FNV32("MNetworkChangeCallback"),
+        FNV32("MPropertyFriendlyName"),
+        FNV32("MPropertyDescription"),
+        FNV32("MPropertyAttributeRange"),
+        FNV32("MPropertyStartGroup"),
+        FNV32("MPropertyAttributeChoiceName"),
+        FNV32("MPropertyGroupName"),
+        FNV32("MNetworkUserGroup"),
+        FNV32("MNetworkAlias"),
+        FNV32("MNetworkTypeAlias"),
+        FNV32("MNetworkSerializer"),
+        FNV32("MPropertyAttributeEditor"),
+        FNV32("MPropertySuppressExpr"),
+        FNV32("MKV3TransferName"),
+        FNV32("MFieldVerificationName"),
+        FNV32("MVectorIsSometimesCoordinate"),
+        FNV32("MNetworkEncoder"),
+        FNV32("MPropertyCustomFGDType"),
+        FNV32("MVDataUniqueMonotonicInt"),
+        FNV32("MScriptDescription"),
+    };
 
     constinit std::array string_class_metadata_entries = {
         FNV32("MResourceTypeForInfoType"),
@@ -484,8 +486,8 @@ namespace sdk {
                 for (const auto& field : class_info->GetFields()) {
                     // @fixme: @es3n1n: todo proper collision fix and remove this block
                     if (state.collision_end_offset && field.m_nSingleInheritanceOffset < state.collision_end_offset) {
-                        builder.comment(
-                            std::format("Skipped field \"{}\" @ {:#x} because of the struct collision", field.m_pszName, field.m_nSingleInheritanceOffset));
+                        builder.comment(std::format("Skipped field \"{}\" @ {:#x} because of the struct collision", field.m_pszName,
+                                                    field.m_nSingleInheritanceOffset));
                         continue;
                     }
 
@@ -503,8 +505,8 @@ namespace sdk {
                     // @note: @es3n1n: insert padding if needed
                     //
                     const auto expected_offset = state.last_field_offset + state.last_field_size;
-                    if (state.last_field_offset && state.last_field_size && expected_offset < static_cast<std::uint64_t>(field.m_nSingleInheritanceOffset) &&
-                        !state.assembling_bitfield) {
+                    if (state.last_field_offset && state.last_field_size &&
+                        expected_offset < static_cast<std::uint64_t>(field.m_nSingleInheritanceOffset) && !state.assembling_bitfield) {
 
                         builder.access_modifier("private")
                             .struct_padding(expected_offset, field.m_nSingleInheritanceOffset - expected_offset, false, true)
