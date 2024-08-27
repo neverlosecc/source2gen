bdcd7e35d9e14995b0e833146384056536e810ad
Cre3per <einstap@gmail.com>
2024-08-27 16:52:09 +0200

    add conan to generated sdk

---
+++ sdk-static/conanfile.py
@@ -0,0 +1,51 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+# TODO: We should set `version` and `name` to reflect what game this sdk is for
+class source2sdkRecipe(ConanFile):
+    name = "source2sdk"
+    version = "0.0.0"
+    package_type = "library"
+
+    author = "source2gen"
+    url = "https://github.com/neverlosecc/source2gen"
+    description = "Source2 SDK"
+    topics = ("source2")
+
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    exports_sources = "CMakeLists.txt", "include/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+    
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
